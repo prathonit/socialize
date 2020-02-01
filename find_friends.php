3282f966231019f1c9f18fe3a5384de415f7265e
prathonit <prathonit@gmail.com>
2020-02-02 00:02:36 +0530

    Update find_friends.php

--- find_friends.php
+++ find_friends.php
@@ -26,7 +26,17 @@
 		</div>
 	</nav>
 	<center>
-				
+		<br><br>
+		<form>
+			<div class="input-group" style="width:50%;">
+				<input type="text" class="form-control" placeholder="Search">
+				<div class="input-group-btn">
+						<button class="btn btn-default" type="submit">
+							<i class="glyphicon glyphicon-search"></i>
+						</button>
+				</div> 
+			</div>
+		</form>		
 		
 	</center>
 </body>
